1a0592437278ab26241476e9f2e9f7fe3352a7ce
omar mohamed <73157064+omareldanin@users.noreply.github.com>
2022-06-12 05:18:17 +0200

    Time: 255 ms (48.84%), Space: 51.6 MB (23.40%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,14 +3,21 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if (x < 0) {
-    return false;
-  } else {
-    let stringNum = x.toString().split("").reverse().join("");
-    if (parseInt(stringNum) === x) {
-        return true;
-    } else {
-      return false;
-    }
-  }
+   if(x === 0)
+        return true
+    
+    const firstNumberToString = String(x)
+    
+    let secondNumberToString = ""
+    
+    while(x >= 1)
+        {
+            let currentDigitToString = String(Math.round(x % 10))
+            
+            secondNumberToString += currentDigitToString
+            
+            x = Math.trunc(x / 10);
+        }
+    
+    return firstNumberToString === secondNumberToString
 };
