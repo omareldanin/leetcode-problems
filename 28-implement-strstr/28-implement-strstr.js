038304ec67653707a8d60d20105f02eecde5d8b5
omar mohamed <73157064+omareldanin@users.noreply.github.com>
2022-06-25 00:28:29 +0200

    Time: 117 ms (9.50%), Space: 43.7 MB (6.67%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+      var result = -1;
+  var x = [];
+  if (needle.length === 0) {
+    return 0;
+  }
+  for (var i = 0; i < haystack.length; i++) {
+    if (needle[0] === haystack[i]) {
+      x.push(i);
+    }
+  }
+  for (let i = 0; i < x.length; i++) {
+    var temp = haystack.slice(x[i], x[i] + needle.length);
+    console.log(temp);
+    if (temp === needle) {
+      result = x[i];
+      break;
+    }
+  }
+  return result;
+};
